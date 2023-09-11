3051f808854ef32255f80d3078cfb9d30e3bea44
Martin <martin.guenduez@efs-auto.com>
2023-09-11 10:41:37 +0200

    <type>: <description>
    ADDED:
    -
    
    CHANGED:
    -
    
    DELETED:
    -
    
    REFS: <storynumber>

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface InputProps {}
+
+export function Input(props: InputProps) {
+  return (
+    <div>
+      <h1>Welcome to Input!</h1>
+    </div>
+  );
+}
+
+export default Input;
